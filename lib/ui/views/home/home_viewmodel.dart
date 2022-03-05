1dabf0f4df6a0d05c2c688d9f2dd3eff4fee5537
Oleg_Kozlov <oleg221094@yandex.ru>
2022-03-05 16:14:46 +0200

    Initial commit

---
+++ lib/ui/views/home/home_viewmodel.dart
@@ -0,0 +1,13 @@
+import 'package:stacked/stacked.dart';
+import 'package:flutter/material.dart';
+
+import '';
+
+class HomeViewModel extends BaseViewModel {
+  String _title = 'Home View';
+  String get title => _title;
+
+  void getUsers() {
+
+  }
+}
